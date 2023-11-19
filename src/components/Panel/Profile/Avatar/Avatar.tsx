78a8396fd7851e2e560f52dee3471a61ea6f6c62
Gregory <not.lost.in.code@gmail.com>
2023-11-19 22:03:57 +0400

    avatar update

--- src/components/Panel/Profile/Avatar/Avatar.tsx
+++ src/components/Panel/Profile/Avatar/Avatar.tsx
@@ -1,19 +1,30 @@
 import React, {ChangeEvent, useState} from 'react';
 import styles from './Avatar.module.css'
 import avatar from "../../../../assets/images/avatar.png";
+import addAvatar from '../../../../assets/icons/addAvatar.svg'
 
 export const Avatar = () => {
     const [avatarUpdate, setAvatarUpdate] = useState(avatar)
 
     const onChangeAvatarUpdate = (e: ChangeEvent<HTMLInputElement>) => {
-
+        if(e.currentTarget.files && e.currentTarget.files[0]) {
+            setAvatarUpdate(URL.createObjectURL(e.currentTarget.files[0]))
+        }
     }
 
     return (
-        <div className={styles.avatar}>
-            <input type="file" style={{display: 'none'}} id={styles.inputFile} onChange={onChangeAvatarUpdate}/>
-            <div><label htmlFor={styles.inputFile}></label></div>
-            <img src={avatarUpdate} alt=""/>
+        <div className={styles.profileAvatar}>
+
+            <div className={styles.avatarUpdate}>
+                <img className={styles.avatar} src={avatarUpdate} alt=""/>
+                <input type="file" style={{display: 'none'}} id={styles.inputFile} onChange={onChangeAvatarUpdate}/>
+                <label htmlFor={styles.inputFile}>
+                    <img src={addAvatar} alt=""/>
+                </label>
+            </div>
+
+
+
         </div>
     );
 };
